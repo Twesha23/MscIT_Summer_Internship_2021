d7c3d62cdf06cfe9234d3da3a54efb7943eead32
Twesha23 <59500955+Twesha23@users.noreply.github.com>
2021-06-06 20:12:20 +0530

    Updates

---
+++ Head.js
@@ -0,0 +1,11 @@
+import './Head.css';
+const Head = (props) =>{
+    return(
+        <div className="f1 tc">
+            
+            <h1>Cafes in Ahmedabad</h1>
+            <p>{props.tagline}</p>
+        </div>
+    );
+}
+ export default Head;
