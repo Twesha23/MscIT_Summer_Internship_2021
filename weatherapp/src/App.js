d26e4a675fd7bf4eb2e1a56a5744cfbc54243966
Twesha23 <59500955+Twesha23@users.noreply.github.com>
2021-06-13 02:44:42 +0530

    weather app-react

---
+++ weatherapp/src/App.js
@@ -0,0 +1,65 @@
+
+import React , { useState } from 'react';
+import './App.css';
+
+const api ={
+  key:"4ca5e2f3d1cc8cd593a61d77d3786d71",
+  base :"https://api.openweathermap.org/data/2.5/"
+};
+function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+  var options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let date = new Date();
+  date = date.toLocaleDateString("en-US", options);
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'App warm' : 'App') : 'App'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{date}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
