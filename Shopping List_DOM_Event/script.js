14525334353d780a77d31bfc2dd8dac5fab014aa
Twesha23 <59500955+Twesha23@users.noreply.github.com>
2021-06-15 01:59:16 +0530

    DOM Manipulation

--- Shopping List_DOM_Event/script.js
+++ Shopping List_DOM_Event/script.js
@@ -1,33 +1,58 @@
-var button=document.getElementsByTagName("button")[0];
-button.addEventListener("click",function(){
-    console.log("clicked");
-})
+// var button=document.getElementsByTagName("button")[0];
+// button.addEventListener("click",function(){
+//     console.log("clicked");
+// })
 
-var button1=document.getElementById("enter");
-var input=document.getElementById("userinput");
-var ul=document.querySelector("ul");
+var addBtn = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var doneBtn = document.querySelectorAll(".checkbtn");
 
-function inputLength()
-{
-    return input.value.length;
+function inputLength() {
+	return input.value.length;
 }
 
-function createListElement()
-{
-    var li=document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li); 
-    input.value="";
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	var btn = document.createElement("button");
+	btn.appendChild(document.createTextNode("Mark as Done"));
+	btn.classList.add("checkbtn");
+	btn.addEventListener("click", markAsDone );
+	li.appendChild(btn);
+	ul.appendChild(li);
+	input.value = "";
 }
 
-function addListAfterKeypress(event)
-{
-    if(inputLength()>0 && event.keyCode === 13)
-    createListElement();
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
-button1.addEventListener("click",function(){
-    if(inputLength() >0)
-        createListElement();
-})
 
-input.addEventListener("keypress",addListAfterKeypress);
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function markAsDone(event){
+	console.log(event);
+	var targetbtn = event.target;
+	var targetli = targetbtn.parentElement;
+	targetli.classList.add("done");
+	
+}
+
+addBtn.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+function onload()
+{
+	doneBtn.forEach(element => {
+		element.addEventListener("click", markAsDone );
+	});
+	
+}
+onload();
