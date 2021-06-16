56029fcc7a11e87a2971566a0f7596822a769fd1
Twesha23 <59500955+Twesha23@users.noreply.github.com>
2021-06-17 03:05:20 +0530

    middleware, server ,bodyparser ,  get , post , put ,delete concepts

--- Express.js_Basics/server.js
+++ Express.js_Basics/server.js
@@ -10,7 +10,23 @@ app.use((req,res,next)=>{
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/',(req,res)=>{
+    console.log(req.query);
+    console.log(req.body);
+    console.log(req.headers);
+    console.log(req.params);
+    res.send("getting root");
+})
+
+app.get('/:id',(req,res)=>{
+    console.log(req.params);
+    res.status(404).send("not found");
+})
+
+
+app.get('/profile',(req,res)=>{
     //res.send('hello')
     const user={
         name:'Twesha',
